b9100cf72427d98a9f489ab395e40deec86a6063
ekeilty17 <epkeilty@hotmail.com>
2021-03-28 21:53:50 -0400

    perfect

--- preliminary_test.py
+++ preliminary_test.py
@@ -67,7 +67,7 @@ def static_primer_set(df, primer_df, GPT2FR, hyperparameters, debug=False, save_
                     "Topic": row["Topic"], 
                     "prompt": row["prompt"], 
                     "response": row["response"], 
-                    "primer_type": "no_primers", 
+                    "primer_type": "none", 
                     "primers": "\n\n".join(examples),
                     "gpt2_input": gpt2_input,
                     "generated_reflection": generated_reflection,
@@ -77,7 +77,6 @@ def static_primer_set(df, primer_df, GPT2FR, hyperparameters, debug=False, save_
                 
             # logging output
             if index % NUM_ITERATIONS == 0:
-                Log += log_print(primer_type)
                 for i, example in enumerate(examples):
                     Log += log_print(f"{i+1} {example}")
                 
@@ -104,6 +103,86 @@ def static_primer_set(df, primer_df, GPT2FR, hyperparameters, debug=False, save_
 
     return output_df
 
+
+def perfect_preformance(df, GPT2FR, hyperparameters, debug=False, save_dir="."):
+
+    N = hyperparameters["num_shots"]
+    NUM_ITERATIONS = 1                  # number of iterations until we print results
+
+    Log = ""
+    output_df = pd.DataFrame(columns=["Type", "Topic", "prompt", "response", "primer_type", "primers", "gpt2_input", "generated_reflection", "beam"] + list(hyperparameters.keys()))
+    try:
+        for index, row in tqdm(df.iterrows()):
+            
+            # logging output
+            if index % NUM_ITERATIONS == 0:
+                Log += log_print()
+                Log += log_print("------------------------------")
+                Log += log_print(f"Iteration: {index}")
+                Log += log_print("------------------------------")
+                Log += log_print()
+            
+            # convert dataframe to list of strings
+            primer_string = GPT2FR.convert_example_to_formatted_string( row["prompt"], row["response"], row["reflection"] )
+            examples = [primer_string for _ in range(5)]
+
+            # convert row we want to generate a reflection of to a string
+            query_string = GPT2FR.convert_example_to_formatted_string( row["prompt"], row["response"] )
+
+            # generating reflection
+            gpt2_input = "\n\n".join(examples + [query_string])
+
+            # generating reflections
+            print("Generating Reflection...")
+            GPT2FR.update_hyperparameters(hyperparameters, hyperparameters["search_type"])
+            generated_reflections = GPT2FR(gpt2_input)
+            
+            total_hp = hyperparameters.copy()
+            total_hp.update(GPT2FR.hyperparameters)
+            
+            for i, generated_reflection in enumerate(generated_reflections, 1):
+                # saving to dictionary
+                hp_str = str(total_hp)
+                output_df = output_df.append({
+                    "Type": row["Type"], 
+                    "Topic": row["Topic"], 
+                    "prompt": row["prompt"], 
+                    "response": row["response"], 
+                    "primer_type": "perfect", 
+                    "primers": "\n\n".join(examples),
+                    "gpt2_input": gpt2_input,
+                    "generated_reflection": generated_reflection,
+                    "beam": i,
+                    **hyperparameters
+                }, ignore_index=True)
+                
+            # logging output
+            if index % NUM_ITERATIONS == 0:
+                for i, example in enumerate(examples):
+                    Log += log_print(f"{i+1} {example}")
+                
+                Log += log_print(query_string)
+                Log += log_print()
+                
+                Log += log_print(str(generated_reflections))
+                Log += log_print()
+
+    except (KeyboardInterrupt, SystemExit):
+        # This way the code will still save the data even if an interrupt occurs
+        pass
+    except Exception as e: 
+        Log += "ERROR" + "\n"
+        Log += str(e) + "\n"
+        print()
+        traceback.print_exc()
+        raise Exception(e)
+
+    # saving log file
+    print("Saving log file...")
+    with open(f"{save_dir}/Log.txt", "w+") as g:
+        g.write(Log)
+
+    return output_df
 
 if __name__ == "__main__":
 
@@ -136,11 +215,13 @@ if __name__ == "__main__":
 
     #primer_file = "simple_reflections_gpt2.csv"
     #primer_file = "complex_reflections_gpt2.csv"
-    primer_file = "simple_reflections_human.csv"
+    #primer_file = "simple_reflections_human.csv"
     #primer_file = "complex_reflections_human.csv"
+    primer_file = "token_examples.csv"
     data_set = pd.read_csv(f'static_data/{primer_file}', index_col=0)
 
     primer_df = pd.DataFrame(columns=["prompt", "response", "reflection"])
 
     SAVE_DIR = "generated_data"
-    df = static_primer_set(data_set, primer_df, GPT2FR, hyperparameters, debug=args.debug, save_dir=SAVE_DIR)
+    #df = static_primer_set(data_set, primer_df, GPT2FR, hyperparameters, debug=args.debug, save_dir=SAVE_DIR)
+    df = perfect_preformance(data_set, GPT2FR, hyperparameters, debug=args.debug, save_dir=SAVE_DIR)
