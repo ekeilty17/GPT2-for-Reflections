6535d838a6fabeaae43c9bc7265014d9ae8387a2
ekeilty17 <epkeilty@hotmail.com>
2021-03-30 00:43:41 -0400

    add permutations

--- k_fold.py
+++ k_fold.py
@@ -28,11 +28,23 @@ def k_fold(df, Primers, GPT2FR, hyperparameters, debug=False, save_dir="."):
     N = hyperparameters["num_shots"]
     NUM_ITERATIONS = 1                  # number of iterations until we print results
 
+    assert N == 5, "Permutations will not work"
+
+    PERMUTATIONS = [
+                [0, 1, 2, 3, 4],
+                [3, 1, 0, 4, 2],
+                [4, 2, 1, 0, 3],
+                [2, 0, 4, 3, 1],
+                [1, 2, 0, 4, 3],
+                [0, 3, 2, 1, 4],
+                [4, 3, 2, 1, 0]
+            ]
+
     Log = ""
     output_df = pd.DataFrame(columns=["Type", "Topic", "prompt", "response", "primer_type", "generated_reflection"] + list(hyperparameters.keys()))
     try:
         for index, row in tqdm(df.iterrows()):
-
+            
             # getting primers
             random_examples = Primers.get_n_random_examples(N)
             similar_examples = Primers.get_n_similar_examples(Primers.get_prompt_response_string(row["prompt"], row["response"]), N)
@@ -45,52 +57,58 @@ def k_fold(df, Primers, GPT2FR, hyperparameters, debug=False, save_dir="."):
             if index % NUM_ITERATIONS == 0:
                 Log += log_print()
                 Log += log_print("------------------------------")
-                Log += log_print(f"Iteration: {index}")
+                Log += log_print(f"Index: {index}")
                 Log += log_print("------------------------------")
                 Log += log_print()
             
             for examples, primer_type in zip([random_examples, similar_examples, different_examples], ["random", "similar", "different"]):
+                examples = np.array(examples)
 
-                # convert dataframe to list of strings
-                examples = [GPT2FR.convert_example_to_formatted_string( ex_row["prompt"], ex_row["response"], ex_row["reflection"] ) \
-                                for _, ex_row in examples.iterrows()]
+                for p in range(permutations):
+                    examples = examples[PERMUTATIONS[p]]
 
-                # generating reflection
-                gpt2_input = "\n\n".join(examples + [query_string])
+                    # convert dataframe to list of strings
+                    examples = [GPT2FR.convert_example_to_formatted_string( ex_row["prompt"], ex_row["response"], ex_row["reflection"] ) \
+                                    for _, ex_row in examples.iterrows()]
 
-                # generating reflections
-                print("Generating Reflection...")
-                GPT2FR.update_hyperparameters(hyperparameters, hyperparameters["search_type"])
-                generated_reflections = GPT2FR(gpt2_input)
-                
-                total_hp = hyperparameters.copy()
-                total_hp.update(GPT2FR.hyperparameters)
-                
-                for i, generated_reflection in enumerate(generated_reflections, 1):
-                    # saving to dictionary
-                    hp_str = str(total_hp)
-                    output_df = output_df.append({
-                        "Type": row["Type"], 
-                        "Topic": row["Topic"], 
-                        "prompt": row["prompt"], 
-                        "response": row["response"], 
-                        "primer_type": primer_type, 
-                        "generated_reflection": generated_reflection,
-                        "beam": i,
-                        **hyperparameters
-                    }, ignore_index=True)
-                
-                # logging output
-                if index % NUM_ITERATIONS == 0:
-                    Log += log_print(primer_type)
-                    for i, example in enumerate(examples):
-                        Log += log_print(f"{i+1} {example}")
+                    # generating reflection
+                    gpt2_input = "\n\n".join(examples + [query_string])
+
+                    # generating reflections
+                    print("Generating Reflection...")
+                    GPT2FR.update_hyperparameters(hyperparameters, hyperparameters["search_type"])
+                    generated_reflections = GPT2FR(gpt2_input)
                     
-                    Log += log_print(query_string)
-                    Log += log_print()
+                    total_hp = hyperparameters.copy()
+                    total_hp.update(GPT2FR.hyperparameters)
                     
-                    Log += log_print(str(generated_reflections))
-                    Log += log_print()
+                    for i, generated_reflection in enumerate(generated_reflections, 1):
+                        # saving to dictionary
+                        hp_str = str(total_hp)
+                        output_df = output_df.append({
+                            "Type": row["Type"], 
+                            "Topic": row["Topic"], 
+                            "prompt": row["prompt"], 
+                            "response": row["response"], 
+                            "primer_type": primer_type, 
+                            "primers": "\n\n".join(examples),
+                            "gpt2_input": gpt2_input,
+                            "generated_reflection": generated_reflection,
+                            "beam": i,
+                            **hyperparameters
+                        }, ignore_index=True)
+                    
+                    # logging output
+                    if index % NUM_ITERATIONS == 0:
+                        Log += log_print(primer_type)
+                        for i, example in enumerate(examples):
+                            Log += log_print(f"{i+1} {example}")
+                        
+                        Log += log_print(query_string)
+                        Log += log_print()
+                        
+                        Log += log_print(str(generated_reflections))
+                        Log += log_print()
 
     except (KeyboardInterrupt, SystemExit):
         # This way the code will still save the data even if an interrupt occurs
@@ -131,9 +149,9 @@ if __name__ == "__main__":
     GPT2FR = GPT2ForReflections(model_name=args.model)
 
     print("\nLoading Primers...")
-    primer_file = "simple_reflections_gpt2.csv"
+    #primer_file = "simple_reflections_gpt2.csv"
     #primer_file = "complex_reflections_gpt2.csv"
-    #primer_file = "simple_reflections_human.csv"
+    primer_file = "simple_reflections_human.csv"
     #primer_file = "complex_reflections_human.csv"
     primer_df = pd.read_csv(f'static_data/{primer_file}', index_col=0)
 
